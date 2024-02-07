8f7a9c5eb5a0fdcc7085bb4cf7b5529e47f018a0
rakesh-viteb <rakeshvijay.singh@gmail.com>
2024-02-07 06:37:04 -1200

    Update BookingController.php

--- refactor/app/Http/Controllers/BookingController.php
+++ refactor/app/Http/Controllers/BookingController.php
@@ -3,7 +3,6 @@
 namespace DTApi\Http\Controllers;
 
 use DTApi\Models\Job;
-use DTApi\Http\Requests;
 use DTApi\Models\Distance;
 use Illuminate\Http\Request;
 use DTApi\Repository\BookingRepository;
@@ -182,9 +181,8 @@ class BookingController extends Controller
      * @param Request $request
      * @return mixed
      */
-    public function getPotentialJobs(Request $request)
+    public function getPotentialJobs()
     {
-        $data = $request->all();
         $user = $request->__authenticatedUser;
 
         $response = $this->repository->getPotentialJobs($user);
@@ -195,59 +193,52 @@ class BookingController extends Controller
     public function distanceFeed(Request $request)
     {
         $data = $request->all();
+        $distance = "";
+        $time = "";
+        $session = "";
+        $flagged = 'no';
+        $manually_handled = 'no';
+        $by_admin = 'no';
+        $admincomment = "";
+
+
 
         if (isset($data['distance']) && $data['distance'] != "") {
             $distance = $data['distance'];
-        } else {
-            $distance = "";
-        }
+        } 
         if (isset($data['time']) && $data['time'] != "") {
             $time = $data['time'];
-        } else {
-            $time = "";
-        }
+        } 
         if (isset($data['jobid']) && $data['jobid'] != "") {
             $jobid = $data['jobid'];
         }
 
         if (isset($data['session_time']) && $data['session_time'] != "") {
             $session = $data['session_time'];
-        } else {
-            $session = "";
-        }
+        } 
 
         if ($data['flagged'] == 'true') {
             if($data['admincomment'] == '') return "Please, add comment";
             $flagged = 'yes';
-        } else {
-            $flagged = 'no';
         }
         
         if ($data['manually_handled'] == 'true') {
             $manually_handled = 'yes';
-        } else {
-            $manually_handled = 'no';
         }
 
         if ($data['by_admin'] == 'true') {
             $by_admin = 'yes';
-        } else {
-            $by_admin = 'no';
-        }
+        } 
 
         if (isset($data['admincomment']) && $data['admincomment'] != "") {
             $admincomment = $data['admincomment'];
-        } else {
-            $admincomment = "";
-        }
+        } 
         if ($time || $distance) {
-
-            $affectedRows = Distance::where('job_id', '=', $jobid)->update(array('distance' => $distance, 'time' => $time));
+             Distance::where('job_id', '=', $jobid)->update(array('distance' => $distance, 'time' => $time));
         }
 
         if ($admincomment || $session || $flagged || $manually_handled || $by_admin) {
-
-            $affectedRows1 = Job::where('id', '=', $jobid)->update(array('admin_comments' => $admincomment, 'flagged' => $flagged, 'session_time' => $session, 'manually_handled' => $manually_handled, 'by_admin' => $by_admin));
+            Job::where('id', '=', $jobid)->update(array('admin_comments' => $admincomment, 'flagged' => $flagged, 'session_time' => $session, 'manually_handled' => $manually_handled, 'by_admin' => $by_admin));
 
         }
 
@@ -281,7 +272,6 @@ class BookingController extends Controller
     {
         $data = $request->all();
         $job = $this->repository->find($data['jobid']);
-        $job_data = $this->repository->jobToData($job);
 
         try {
             $this->repository->sendSMSNotificationToTranslator($job);
